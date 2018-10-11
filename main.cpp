8f0ff4360d47615bcee41200592731e98a48729a
uladzimir zhytkouski <uladzimir.zhytkouski@psa-software.com>
2018-10-11 14:01:28 +0300

    Modified output to the screen 4

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include "lib.h"
-#include <iostream>
-
 #include <spdlog/spdlog.h>
 
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("\nbuild {} {}", version(),"\nHello, World!\n");
+    logger->info("\n\nbuild {} {}", version(),"\nHello, World!\n");
 
     return 0;
 }
