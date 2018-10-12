b348ec6330ed2b2220686a663c0a4070e318483e
uladzimir zhytkouski <uladzimir.zhytkouski@psa-software.com>
2018-10-12 10:17:06 +0300

    Modified output to the screen 6

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
     logger->set_pattern("%v");
-    logger->info("\nbuild {} {}", version(),"\nHello, World!\n");
+    logger->info("build {}", version());
+	logger->info("Hello, World!")
 
     return 0;
 }
